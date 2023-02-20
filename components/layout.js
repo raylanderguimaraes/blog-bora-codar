9310972f74293793df8304bc558cdd2ac752719a
Raylander <raylander.guimaraes@gmail.com>
2023-02-20 11:27:16 -0300

    adiciona meta de verificacao

--- components/layout.js
+++ components/layout.js
@@ -27,11 +27,11 @@ export default function Layout({ children, home }) {
                     property="og:image"
                     content={`https://og-image.vercel.app/${encodeURI(siteTitle,)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
-
+                <meta name="google-site-verification" content="unsrrrGt9IHb_29SXV5BIe-mqqbrCH_8_EQnD3fWfMU" />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
                 <Script
-                    
+
                 />
             </Head>
 
